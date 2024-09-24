a0b03baaf6816cc036180f7dc0598268198da8c2
sidstedt <daniel.sidstedt@gmail.com>
2024-09-24 14:25:52 +0200

    added some numpad display

--- GroupCalc/Program.cs
+++ GroupCalc/Program.cs
@@ -9,9 +9,9 @@
             {
                 Console.WriteLine("Välkommen till GJD's fräcka kalkylator!");
                 Console.WriteLine("Välj två tal och sedan vad du ska göra med dom. Skriv ditt första tal");
-                int a = Convert.ToInt32(Console.ReadLine());
+                int a = SelectNumber();
                 Console.WriteLine("Skriv ditt andra tal.");
-                int b = Convert.ToInt32(Console.ReadLine());
+                int b = SelectNumber();
                 Console.WriteLine();
                 Console.WriteLine("Välj ett menyalternativ!");
                 Console.WriteLine("" +
@@ -73,6 +73,66 @@
         {
             int quota = number / divider;
             return quota;
+        }
+        // OBS! AI-GENERATED OBS! //
+        static int SelectNumber()
+        {
+            char[,] numpad = new char[,]
+            {
+                { '7', '8', '9' },
+                { '4', '5', '6' },
+                { '1', '2', '3' },
+                { ' ', '0', ' ' }
+            };
+
+            int selectedNumber = -1; // Keep track of which number is highlighted
+            bool selecting = true;
+
+            while (selecting)
+            {
+                Console.Clear();
+                Console.WriteLine("Tryck på en siffra (0-9) och tryck på Enter för att bekräfta.");
+
+                // Display numpad and highlight the selected number
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (selectedNumber == (numpad[i, j] != ' ' ? int.Parse(numpad[i, j].ToString()) : -1))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green; // Highlight selected number
+                        }
+                        else
+                        {
+                            Console.ResetColor();
+                        }
+
+                        Console.Write(numpad[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ResetColor();
+
+                // Read user input
+                var keyPressed = Console.ReadKey(true);
+                
+                // Check if it's a number key
+                if (char.IsDigit(keyPressed.KeyChar))
+                {
+                    selectedNumber = int.Parse(keyPressed.KeyChar.ToString()); // Highlight the number
+                }
+                else if (keyPressed.Key == ConsoleKey.Enter && selectedNumber != -1)
+                {
+                    selecting = false; // Confirm the selection when Enter is pressed
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltig inmatning. Vänligen välj ett nummer mellan 0 och 9.");
+                }
+            }
+
+            return selectedNumber;
         }
     }
 }
