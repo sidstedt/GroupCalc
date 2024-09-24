923eb0ae17544e86be0e33e12e36fe890e9ab8bb
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2024-09-24 15:55:06 +0200

    In progress

--- GroupCalc/Program.cs
+++ GroupCalc/Program.cs
@@ -47,7 +47,7 @@
                 }
             }
 
-            
+
 
 
 
@@ -81,23 +81,94 @@
             return quota;
         }
 
+        static void ViewDisplay()
+        {
+            string[][] display = new string[5][];
+
+            string[][] test = 
+            {
+                new string[]
+                {
+                    "XXXXX",
+                    "X   X",
+                    "X i X",
+                    "X   X",
+                    "XXXXX",
+                },
+                new string[]
+                {
+                    " XX  ",
+                    "X X  ",
+                    "  X  ",
+                    "  X  ",
+                    "XXXXX"
+                }
+            };
+            string[] testigen = ["XXXXX", "X   X", "X   X", "X   X", "XXXXX"];
+            int slot = 0;
+            
+            while(slot <= 1)
+            {
+
+            ConsoleKeyInfo cki = Console.ReadKey(true);
 
 
+
+            if(cki.Key == ConsoleKey.D0)
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    test[slot][i] = testigen[i];
+                }
+                
+            }
+                slot++;
+
+                
+            }
+            
+            
+
+            // display = DisplayNumber(cki);
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int column = i; column <= i; column++)
+                {
+                    Console.Write(test[0][i]);
+                    Console.Write(" ");
+                    Console.Write(test[1][i]);
+                }
+
+
+                Console.WriteLine(" ");
+               
+                // Console.Write(what);
+                
+                
+            }
+
+
+
+
+        }
+
+        
 
         static string[][] DisplayNumber(ConsoleKeyInfo x)
         {
             string[][] convert = new string[5][];
 
-            switch(x.Key)
+            switch (x.Key)
             {
                 case ConsoleKey.D0:
 
-                    convert[0] = [ "X","X","X","X","X" ];
-                    convert[1] = [ "X"," "," "," ","X" ];
-                    convert[2] = [ "X"," "," "," ","X" ];
-                    convert[3] = [ "X"," "," "," ","X" ];
-                    convert[4] = [ "X","X","X","X","X" ];
-                   
+                    convert[0] = ["X", "X", "X", "X", "X"];
+                    convert[1] = ["X", " ", " ", " ", "X"];
+                    convert[2] = ["X", " ", " ", " ", "X"];
+                    convert[3] = ["X", " ", " ", " ", "X"];
+                    convert[4] = ["X", "X", "X", "X", "X"];
+
 
                     break;
 
@@ -112,7 +183,7 @@
 
                     break;
 
-                    case ConsoleKey.D2:
+                case ConsoleKey.D2:
 
                     convert[0] = ["X", "X", "X", "X", "X"];
                     convert[1] = [" ", " ", " ", " ", "X"];
@@ -122,7 +193,7 @@
 
 
                     break;
-                    case ConsoleKey.D3:
+                case ConsoleKey.D3:
 
                     convert[0] = ["X", "X", "X", "X", "X"];
                     convert[1] = [" ", " ", " ", " ", "X"];
@@ -131,7 +202,7 @@
                     convert[4] = ["X", "X", "X", "X", "X"];
 
                     break;
-                    case ConsoleKey.D4:
+                case ConsoleKey.D4:
                     convert[0] = ["X", " ", " ", " ", "X"];
                     convert[1] = ["X", " ", " ", " ", "X"];
                     convert[2] = ["X", "X", "X", "X", "X"];
@@ -139,7 +210,7 @@
                     convert[4] = [" ", " ", " ", " ", "X"];
                     break;
 
-                    case ConsoleKey.D5:
+                case ConsoleKey.D5:
                     convert[0] = ["X", "X", "X", "X", "X"];
                     convert[1] = ["X", " ", " ", " ", " "];
                     convert[2] = ["X", "X", "X", "X", "X"];
@@ -147,7 +218,7 @@
                     convert[4] = ["X", "X", "X", "X", "X"];
                     break;
 
-                    case ConsoleKey.D6:
+                case ConsoleKey.D6:
                     convert[0] = ["X", "X", "X", "X", "X"];
                     convert[1] = ["X", " ", " ", " ", " "];
                     convert[2] = ["X", "X", "X", "X", "X"];
@@ -155,28 +226,28 @@
                     convert[4] = ["X", "X", "X", "X", "X"];
                     break;
 
-                    case ConsoleKey.D7:
+                case ConsoleKey.D7:
                     convert[0] = ["X", "X", "X", "X", "X"];
                     convert[1] = [" ", " ", " ", " ", "X"];
                     convert[2] = [" ", " ", " ", " ", "X"];
                     convert[3] = [" ", " ", " ", " ", "X"];
                     convert[4] = [" ", " ", " ", " ", "X"];
                     break;
-                    case ConsoleKey.D8:
+                case ConsoleKey.D8:
                     convert[0] = ["X", "X", "X", "X", "X"];
                     convert[1] = ["X", " ", " ", " ", "X"];
                     convert[2] = ["X", "X", "X", "X", "X"];
                     convert[3] = ["X", " ", " ", " ", "X"];
                     convert[4] = ["X", "X", "X", "X", "X"];
                     break;
-                    case ConsoleKey.D9:
+                case ConsoleKey.D9:
                     convert[0] = ["X", "X", "X", "X", "X"];
                     convert[1] = ["X", " ", " ", " ", "X"];
                     convert[2] = ["X", "X", "X", "X", "X"];
                     convert[3] = [" ", " ", " ", " ", "X"];
                     convert[4] = [" ", " ", " ", " ", "X"];
                     break;
-                    default:
+                default:
                     break;
 
 
